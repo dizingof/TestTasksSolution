cd53c2c52359d3028f67a296b2fda5f2b2922a94
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-09-22 23:57:17 +0300

    question 2 done

---
+++ Question2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stringsOne = new[] { "aaa", "bbb", "ccc" };
+            var sb = new StringBuilder();
+            stringsOne.Select(p => p).ToList().ForEach(q => sb.Append(q + " "));
+            Console.WriteLine(sb);
+            Console.ReadKey();
+
+        }
+    }
+}
